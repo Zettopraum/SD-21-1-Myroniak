51efc034f43aeb0e30a27528ac3647bd74616696
Ілля Мироняк <illia.v.myroniak@ukd.edu.ua>
2024-03-10 22:48:50 +0200

    first commit

---
+++ practical-3-1,2.py
@@ -0,0 +1,13 @@
+for i in range(1, 11): 
+    print(i)
+
+for d in range(10, 0, -1):
+    print(d)
+
+for s in range(2,21,2):
+    print(s)
+
+for a in range(1, 11):
+    for b in range(1, 11):
+        result= a * b
+        print(f'{a}x{b}={result}')
---
+++ practical-3.4.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * self.radius ** 2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+circle = Circle(5)
+print("Площа кола:", circle.area())
+
+rectangle = Rectangle(4, 6)
+print("Площа прямокутника:", rectangle.area())
